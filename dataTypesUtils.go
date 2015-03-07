f43a7d0dc54e915c1a3bebec13260656b98dcb85
silviucm <silviucapota@hotmail.com>
2015-03-07 11:06:29 -0500

    split data types functionality from column file to a separate file

---
+++ dataTypesUtils.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"strings"
+)
+
+/* Utility methods for dealing with SQL data types in general and PostgreSQL data types in particular */
+
+func GetGoFriendlyNameForColumn(columnName string) string {
+
+	// find if the table name has underscore
+	if strings.Contains(columnName, "_") == false {
+		return strings.Title(columnName)
+	}
+
+	subNames := strings.Split(columnName, "_")
+
+	if subNames == nil {
+		log.Fatal("GetGoFriendlyNameForColumn() fatal error for column name: ", columnName, ". Please ensure a valid column name is provided.")
+	}
+
+	for i := range subNames {
+		subNames[i] = strings.Title(subNames[i])
+	}
+
+	return strings.Join(subNames, "")
+}
+
+func GetGoTypeForColumn(columnType string) (typeReturn string, goTypeToImport string) {
+
+	typeReturn = ""
+	goTypeToImport = ""
+
+	switch columnType {
+	case "character varying":
+		typeReturn = "string"
+	case "integer":
+		typeReturn = "int"
+	case "boolean":
+		typeReturn = "bool"
+	case "uuid":
+		typeReturn = "string"
+	case "bigint":
+		typeReturn = "int64"
+	case "timestamp with time zone":
+		typeReturn = "time.Time"
+		goTypeToImport = "time"
+	}
+
+	return typeReturn, goTypeToImport
+}
+
+func DecodeIsColumnSequence(columnDefaultValue sql.NullString) bool {
+
+	if columnDefaultValue.Valid == false {
+		return false
+	}
+
+	if strings.HasPrefix(columnDefaultValue.String, "nextval(") {
+		return true
+	}
+
+	return false
+}
+
+func DecodeNullable(isNullable string) bool {
+
+	if isNullable == "NO" {
+		return false
+	}
+
+	if isNullable == "YES" || isNullable == "Yes" || isNullable == "yes" {
+		return true
+	}
+
+	return false
+}
+
+func DecodeMaxLength(maxLength sql.NullInt64) int {
+
+	if maxLength.Valid == false {
+		return -1
+	}
+
+	return int(maxLength.Int64)
+}
