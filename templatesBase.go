f4b1c78da157d4a3a21cfb30c464c46928d606f4
silviucm <silviucapota@hotmail.com>
2015-03-28 10:02:05 -0400

    added support for parameters and return value for TxWrap

--- templatesBase.go
+++ templatesBase.go
@@ -194,13 +194,58 @@ func TxBeginIso(isolationLevel string) (*Transaction, error) {
 
 }
 
-func TxWrap(wrapperFunc func(tx *Transaction) error) error {
+/* This method helps wrap the transaction inside a closure function. Additional arguments can be passed
+ along to the closure via a variadic list of interface{} parameters. 
+ TxWrap automatically handles commit and rollback, in case of error. 
+ It returns an error in case of failure, or nil, in case of success.
+
+ Example:
+
+	// define the transaction functionlity in this wrapper closure
+	var transactionFunc = func(tx *models.Transaction, arguments ...interface{}) (interface{}, error) {
+
+		// assuming the generated package is named models and
+		// there is a TestEvent struct corresponding to a test_event table in the database
+		newTestEvent := models.Tables.TestEvent.New()
+
+		// load the event name as passed via the variadic arguments
+		newTestEvent.SetEventName(arguments[0].(string))
+		newTestEvent.SetEventOverview(arguments[1].(string), true)
+
+		newTestEvent, err := tx.InsertTestEvent(newTestEvent)
+		if err != nil {
+			return nil, models.NewModelsError("insert event tx error:", err)
+		}
+
+		// any other transaction operations...
+
+		// at the end, we return nil for a successful operation
+		return newTestEvent, nil
+	}
+
+	// define some parameters to be passed inside the transaction
+	eventName := "Donald Duck Anniversary"
+	eventDescription := "Where is the party ?"
+
+	// we defined the transaction functionality, let's run it with the event name argument
+	returnedNewEvent, err := models.TxWrap(transactionFunc, eventName, eventDescription)
+	if err != nil {
+		fmt.Println("FAIL:", err.Error())
+	} else {
+		if returnedNewEvent == nil {
+			fmt.Printf("OK. But newlyInsertedEvent is nil \r\n")
+		} else {
+			// we need to make sure to convert the resulting type to the needs of this particular transaction
+			fmt.Printf("OK. newlyInsertedEvent overview: " + returnedNewEvent.(*models.TestEvent).EventOverview + "  \r\n")
+		}
+	} */
+func TxWrap(wrapperFunc func(tx *Transaction, args ...interface{}) (interface{}, error), arguments ...interface{}) (interface{}, error) {
 
 	var errorPrefix = "TxWrap() ERROR: "
 
 	realTx, err := GetDb().Begin()
 	if err != nil {
-		return NewModelsError(errorPrefix+"GetDb().Begin() error: ", err)
+		return nil, NewModelsError(errorPrefix+"GetDb().Begin() error: ", err)
 	}
 
 	// pgx package note: Rollback is safe to call even if the tx is already closed,
@@ -210,17 +255,18 @@ func TxWrap(wrapperFunc func(tx *Transaction) error) error {
 	// wrap the real tx into our wrapper
 	tx := &Transaction{Tx: realTx}
 
-	err = wrapperFunc(tx)
+	result, err := wrapperFunc(tx, arguments...)
 	if err != nil {
-		return NewModelsError(errorPrefix+"inner wrapperFunc() error - will return and rollback: ", err)
+		return nil, NewModelsError(errorPrefix+"inner wrapperFunc() error - will return and rollback: ", err)
 	}
 
 	err = realTx.Commit()
 	if err != nil {
-		return NewModelsError(errorPrefix+"tx.Commit() error: ", err)
+		return nil, NewModelsError(errorPrefix+"tx.Commit() error: ", err)
 	}
 
-	return nil
+	return result, nil
+
 
 }
 
